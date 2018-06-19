0cc43c4ffaf0c4e555351fa441eb7603b8d124f0
Cristiane Silva <cristianeavelarmaia@gmail.com>
2018-06-19 01:02:32 +0000

    Refactored char count after code review

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,14 +1,16 @@
-$(document).ready(function() {
-  $('textarea').on('keyup', function() {
-    let maxLength = 140;
-    let currentLength = $(this).val().length;
+$(function() {
+  const maxLength = 140;
+  const $newTweet = $('.new-tweet');
+  const $counter = $newTweet.find('.counter');
+  $newTweet.find('textarea').on('keyup', function() {
+    const currentLength = $(this).val().length;
     if( currentLength <= maxLength ){
-      $(this).siblings('.counter').text(`${maxLength - currentLength}`);
-      //console.log(maxLength - currentLength + " characters left");
-    }else if (currentLength > maxLength){
-      //console.log(- (currentLength - maxLength) );
-      $(this).siblings('.counter').text(` ${-(currentLength - maxLength)}`).css('color', 'red');
+      $counter.text(`${maxLength - currentLength}`);
+    }else if (currentLength > maxLength) {
+      $counter.text(` ${-(currentLength - maxLength)}`).css('color', 'red');
     }
+  });
 });
 
-})
+
+
