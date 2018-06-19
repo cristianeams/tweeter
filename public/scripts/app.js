9d5cab15dc3acc98fdfab5c48d9cf71dca34a807
Cristiane Silva <cristianeavelarmaia@gmail.com>
2018-06-19 21:50:50 +0000

    Implemented render tweets function

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,42 +1,99 @@
-const tweetData = {
-  "user": {
-    "name": "Newton",
-    "avatars": {
-      "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-      "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-      "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
+// const tweetData = {
+//   "user": {
+//     "name": "Newton",
+//     "avatars": {
+//       "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
+//       "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
+//       "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
+//     },
+//     "handle": "@SirIsaac"
+//   },
+//   "content": {
+//     "text": "If I have seen further it is by standing on the shoulders of giants"
+//   },
+//   "created_at": 1461116232227
+// }
+
+// Fake data taken from tweets.json
+const data = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": {
+        "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
+        "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
+        "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
+      },
+      "handle": "@SirIsaac"
     },
-    "handle": "@SirIsaac"
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
   },
-  "content": {
-    "text": "If I have seen further it is by standing on the shoulders of giants"
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": {
+        "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
+        "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
+        "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
+      },
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
   },
-  "created_at": 1461116232227
-}
+  {
+    "user": {
+      "name": "Johann von Goethe",
+      "avatars": {
+        "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
+        "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
+        "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
+      },
+      "handle": "@johann49"
+    },
+    "content": {
+      "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
+    },
+    "created_at": 1461113796368
+  }
+];
 $(function() {
 
-function createTweetElement() {
-  let $tweet = $("<article>").addClass("tweet");
-  let newHTML = `<header>
-                 <img src=${tweetData.user.avatars.small} alt="User Avatar"/>
-                 <h2>${tweetData.user.name}</h2>
-                 <span class="handle">${tweetData.user.handle}</span>
-                 </header>
+    function renderTweets(tweets) {
+    let $div = $("<div></div>");
+    tweets.forEach(tweet => {
+      let single = createTweetElement(tweet);
+      $div = $div.append(single);
+      return $div;
+    });
 
-                 <p>${tweetData.content.text}</p>
+    $('#tweets-container').append($div);
+  }
 
-                 <footer class="date">${tweetData.created_at}<span id="icons"><i class="fas fa-flag"></i><i class="fas fa-retweet"></i><i class="fas fa-heart"></i></span>
-                 </footer>`;
-  console.log(newHTML);
+  function createTweetElement(tweet) {
+    let $tweet = $("<article>").addClass("tweet");
+    let newHTML = `<header>
+                   <div class="left-elm">
+                   <img src=${tweet.user.avatars.small} alt="User Avatar"/>
+                   <h2>${tweet.user.name}</h2>
+                   </div>
+                   <div class="right-elm">
+                   <span class="handle">${tweet.user.handle}</span>
+                   </div>
+                   </header>
 
-  $tweet = $tweet.append(newHTML);
-  return $tweet;
-}
+                   <p>${tweet.content.text}</p>
 
-let $tweet = createTweetElement(tweetData);
-  console.log($tweet);
+                   <footer class="date">${tweet.created_at}<span id="icons"><i class="fas fa-flag"></i><i class="fas fa-retweet"></i><i class="fas fa-heart"></i></span>
+                   </footer>`;
 
-// Test / driver code (temporary)
-$('#tweets-container').append($tweet);
+    $tweet = $tweet.append(newHTML);
+    return $tweet;
+  }
+renderTweets(data);
 
-});
+ });
