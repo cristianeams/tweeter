fe7900858beb054ec3afa670a908251d58c9fabf
Cristiane Silva <cristianeavelarmaia@gmail.com>
2018-06-20 21:02:41 +0000

    Refetch tweets working. Implemented toggle on button

--- public/scripts/ajax-post.js
+++ public/scripts/ajax-post.js
@@ -1,43 +1,49 @@
 
-$(function() {
-    $("#newTweet-form").submit(function(event){
-        event.preventDefault();
+// $(function() {
+//     $("#newTweet-form").submit(function(event){
+//         event.preventDefault();
 
-        var result = validateForm();//prevents page from refreshing
-        if(result){
-            let post_url = $(this).attr("action"); //get form action
-            let request_method = $(this).attr("method"); //get form method
-            let form_data = $(this).serialize(); //Encode form elements for submission
+//         let result = validateForm();//prevents page from refreshing
 
-            event.preventDefault();
-            console.log(post_url, request_method);
-            console.log(form_data);
-            $.ajax({
-                url : post_url,
-                type: request_method,
-                data : form_data,
-                success: function(result){
-                    $("tweets-container").html(result);
-                },
-                error: function(error){
-                    console.log("Error: ", error);
-                }
-            });
-        }
-    });// function onFormSubmit Ends here;
+//         if(result){
+//             let post_url = $(this).attr("action"); //get form action
+//             let request_method = $(this).attr("method"); //get form method
+//             let form_data = $(this).serialize(); //Encode form elements for submission
 
-    function validateForm(){
-        let validation = false;
-        let message = $('textarea').val();
+//             event.preventDefault();
 
-        if(message.length < 1){
-            $('textarea').after('<span class="error"> You message is empty!</span>');
-        } else if(message.length > 140){
-            $('textarea').after('<span class="error"> You message is too long!</span>');
-        } else {
-            validation = true;
-            return validation;
-        }
-    }
+//             $.ajax({
+//                 url : post_url,
+//                 type: request_method,
+//                 data : form_data
+//             })
+//                 .done (function(result){
+//                     console.log(result)
+//                      $("#tweets-container").load(loadTweets());
+//                      $('#newTweet-form').val('');
+
+//             });
+//         }
+//     });// function onFormSubmit Ends here;
+
+//     function validateForm(){
+//         let validation = false;
+//         let message = $('textarea').val();
+
+//         if(message.length < 1){
+//             $('textarea').after('<span class="error"> You message is empty!</span>');
+//         } else if(message.length > 140){
+//             $('textarea').after('<span class="error"> You message is too long!</span>');
+//         } else {
+//             validation = true;
+//             return validation;
+//         }
+//     }
+//    ///end of validateForm function
+
+//     $( "button#compose" ).click(function() {
+//         $( "section.new-tweet" ).slideToggle();
+//         $('textarea').focus();
+//     });
 
 }); //document ready function
--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,15 +1,15 @@
 $(function() {
 
   function renderTweets(tweets) {
+
     let $div = $("<div></div>");
     tweets.forEach(tweet => {
-      let single = createTweetElement(tweet);
-      $div = $div.append(single);
+      let $single = createTweetElement(tweet);
+      $div = $div.prepend($single);
       return $div;
     });
-
-  $('#tweets-container').append($div);
-  }
+  $('#tweets-container').html($div);
+  } // end of renderTweets function
 
   function createTweetElement(tweet) {
     let $tweet = $("<article>").addClass("tweet");
@@ -30,17 +30,58 @@ $(function() {
 
     $tweet = $tweet.append(newHTML);
     return $tweet;
-  }
+  } //end of createTweetElement function
 
   function loadTweets() {
-    $.ajax({
-      url: '/tweets',
-      method: 'GET',
-      success: renderTweets
-    });
-
-  }
-
+      $.ajax({
+        url: '/tweets',
+        method: 'GET',
+        success: renderTweets
+      });
+  } // end of loadTweets function
   loadTweets();
+
+  $("#newTweet-form").submit(function(event){
+    event.preventDefault();
+    let result = validateForm();
+
+    if(result){
+      let post_url = $(this).attr("action"); //get form action
+      let request_method = $(this).attr("method"); //get form method
+      let form_data = $(this).serialize(); //Encode form elements for submission
+
+      event.preventDefault();
+
+      $.ajax({
+        url : post_url,
+        type: request_method,
+        data : form_data
+        })
+        .done (function(result){
+            $("#tweets-container").load(loadTweets());
+            $('#newTweet-form').val('');
+        });
+    }
+  });// end of onFormSubmit function
+
+  function validateForm(){
+    let validation = false;
+    let message = $('textarea').val();
+
+    if(message.length < 1){
+      $('textarea').after('<span class="error"> You message is empty!</span>');
+      } else if(message.length > 140){
+      $('textarea').after('<span class="error"> You message is too long!</span>');
+      } else {
+        validation = true;
+        return validation;
+      }
+    }
+   ///end of validateForm function
+
+  $( "button#compose" ).click(function() {
+    $( "section.new-tweet" ).slideToggle();
+    $('textarea').focus();
+  });
 
 });
